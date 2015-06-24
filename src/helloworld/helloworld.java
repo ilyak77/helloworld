f6ea977a699f829694f8d9dd0f73b62d54309a8a
yilun zhang <yilun.zhang.07@gmail.com>
2015-06-24 15:12:51 +0800

    check2

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class helloworld {
 	public static void main(String[] args) {
 	   	System.out.println("Hello world");
-	   	System.out.println("check again");
+	   	System.out.println("check again2");
 	   }
 }
